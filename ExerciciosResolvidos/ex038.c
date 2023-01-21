34ce0091ecd96f066174dce16f292606b290ebfa
Renato Gomes de Sousa <renattoengineer@gmail.com>
2023-01-21 18:39:50 -0300

    Exercício 038
    Leia uma data de nascimento de uma pessoa fornecida através de três números inteiros: Dia, Mês e Ano. Teste a validade desta data para saber se esta é uma data válida. Teste se o dia fornecido é um dia válido: dia > 0, dia ≤ 28 para o mês de fevereiro (29 se o ano for bissexto), dia ≤ 30 em abril, junho, setembro e novembro, dia ≤ 31 nos outros meses. Teste a validade do mês: mês > 0 e mês < 13. Teste a validade do ano: ano ≤ ano atual (use uma constante definida com o valor igual a 2008). Imprimir: “Data válida” ou “Data inválida” no final da execução do programa.

---
+++ ExerciciosResolvidos/ex038.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main(){
+
+	int dia;
+	int mes;
+	int ano;
+	int anoAtual = 2023;
+
+	printf("Digite o dia: ");
+	fflush(stdout);
+	scanf("%d", &dia);
+
+	printf("Digite o mes: ");
+	fflush(stdout);
+	scanf("%d", &mes);
+
+	printf("Digite o ano: ");
+	fflush(stdout);
+	scanf("%d", &ano);
+
+	if(ano <= anoAtual){
+		if((ano % 4) == 0 && (ano % 100) != 0){
+			if(dia > 31 || mes > 12){
+				printf("\nData Invalida");
+			}else{
+				if((mes == 4 || mes == 6 || mes == 9 || mes == 11) && (dia < 0 || dia > 30)){
+					printf("\nData Invalida");
+				}else{
+					if(mes == 2 && dia > 29){
+						printf("\nData Invalida");
+					}else{
+						printf("\nData Valida");
+					}
+				}
+			}
+		}else{
+			if(dia > 31 || mes > 12){
+				printf("\nData Invalida");
+			}else{
+				if((mes == 4 || mes == 6 || mes == 9 || mes == 11) && (dia < 0 || dia > 30)){
+					printf("\nData Invalida");
+				}else{
+					if(mes == 2 && dia > 28){
+						printf("\nData Invalida");
+					}else{
+						printf("\nData Valida");
+					}
+				}
+			}
+		}
+	}else{
+		printf("\nData Invalida");
+	}
+
+	return 0;
+}
