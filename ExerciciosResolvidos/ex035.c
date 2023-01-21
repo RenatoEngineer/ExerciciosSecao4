ac86de93333a6f31754846cac810e29d75951d80
Renato Gomes de Sousa <renattoengineer@gmail.com>
2023-01-21 18:31:11 -0300

    Exercício 035
    Leia uma data e determine se ela é válida. Ou seja, verifique se o mês está entre 1 e 12, e se o dia existe naquele mês. Note que Fevereiro tem 29 dias em anos bissextos, e 28 dias em anos não bissextos.

--- ExerciciosResolvidos/ex035.c
+++ ExerciciosResolvidos/ex035.c
@@ -6,30 +6,47 @@ int main(){
 	int mes;
 	int ano;
 
-	printf("Entre com uma data dd/mm/aaaa: ");
+	printf("Digite o dia: ");
 	fflush(stdout);
-	scanf("%d/%d/%d", &dia, &mes, &ano);
+	scanf("%d", &dia);
 
-	if(ano % 400 == 0 || (ano % 4 == 0 && ano % 100 != 0)){
-		if(dia > 31 && mes > 12)
-			printf("Data Invalida\n");
-		else if((mes == 4 || mes == 6 || mes == 9 || mes == 11) && (dia < 0 || dia > 30))
-			printf("Data Invalida\n");
-		else if(mes == 2 && dia > 29)
-				printf("Data Invalida\n");
-		else
-			printf("Data Valida\n");
+	printf("Digite o mes: ");
+	fflush(stdout);
+	scanf("%d", &mes);
+
+	printf("Digite o ano: ");
+	fflush(stdout);
+	scanf("%d", &ano);
+
+	if((ano % 4) == 0 && (ano % 100) != 0){
+		if(dia > 31 || mes > 12){
+			printf("\nData Invalida");
+		}else{
+			if((mes == 4 || mes == 6 || mes == 9 || mes == 11) && (dia < 0 || dia > 30)){
+				printf("\nData Invalida");
+			}else{
+				if(mes == 2 && dia > 29){
+					printf("\nData Invalida");
+				}else{
+					printf("\nData Valida");
+				}
+			}
+		}
 	}else{
-		if(dia > 31 && mes > 12)
-			printf("Data Invalida\n");
-		else if((mes == 4 || mes == 6 || mes == 9 || mes == 11) && (dia < 0 || dia > 30))
-			printf("Data Invalida\n");
-		else if(mes == 2 && dia > 28)
-				printf("Data Invalida\n");
-		else
-			printf("Data Valida\n");
+		if(dia > 31 || mes > 12){
+			printf("\nData Invalida");
+		}else{
+			if((mes == 4 || mes == 6 || mes == 9 || mes == 11) && (dia < 0 || dia > 30)){
+				printf("\nData Invalida");
+			}else{
+				if(mes == 2 && dia > 28){
+					printf("\nData Invalida");
+				}else{
+					printf("\nData Valida");
+				}
+			}
+		}
 	}
-
 
 	return 0;
 }
